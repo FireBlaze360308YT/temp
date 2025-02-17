83b5a5031ff4c661db929619469c429f03d3fd02
FireBlaze360308 <x.block360308@gmail.com>
2025-02-17 16:29:17 +0100

    Update collatz_calculator.py

--- collatz_calculator.py
+++ collatz_calculator.py
@@ -1,10 +1,8 @@
-import os
 error: int = 0
 
 def collatz_calculator(start: int = 1, end: int = 1000, steps: int = 1) -> int:
     global error
     for i in range(start, end + 1, steps):
-        print(f"The initial value of i is {i}.")
         error = i
         seen: set[int] = set()
         while i != 1:
@@ -12,11 +10,9 @@ def collatz_calculator(start: int = 1, end: int = 1000, steps: int = 1) -> int:
                 i //= 2
             else:
                 i = i * 3 + 1
-            print(f"The value of i is {i}")
             if i in seen:
                 return 0
             seen.add(i)
-        print(f"Ending value of i is {i}")
         seen.clear()
 
 def get_positive_integer(prompt: str) -> int:
@@ -26,9 +22,8 @@ def get_positive_integer(prompt: str) -> int:
             choice: int = int(choice)
             if choice >= 1:
                 return choice
-            print("The number must be greater or equal to 1.")
         except ValueError:
-            print("Invalid input. Only positive integers are allowed.")
+            ...
 
 def handle_collatz_choice(choice: int) -> None:
     match choice:
@@ -47,9 +42,6 @@ def handle_collatz_choice(choice: int) -> None:
 
 def selection() -> None:
     while True:
-        os.system('cls')
-        choice: str = input("Collatz calculator\n==================\n1) Fixed range\n2) Custom range\n3) Singular number\nq to quit\n==================\n\nEnter 1, 2, 3 or q\n>>> ").strip().lower()
-
         if choice == "q" or choice == "":
             break
 
@@ -58,4 +50,4 @@ def selection() -> None:
             handle_collatz_choice(choice)
             break
         except ValueError:
-            print("Invalid input. Please enter 1, 2, 3, or 'q' to quit.")
+            ...

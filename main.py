790ac824b5b1471ec847ef2d0bda97e2b45d1f46
FireBlaze360308 <x.block360308@gmail.com>
2025-02-17 16:29:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from collatz_calculator import selection
+from tkinter import ttk
+import tkinter as tk
 
 def main() -> None:
+    window = tk.Tk()
+    window.title("Fire Application")
+    window.geometry("400x300+960+540")
+    window.resizable(False, False)
+   
+    window.iconbitmap("definitivo.ico")
+
+    button = ttk.Button(window, text="IDK")
+    button.pack()
+    window.mainloop()
+
+
     selection()
 
 if __name__ == '__main__':
